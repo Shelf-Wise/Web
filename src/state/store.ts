f4e6ed6b90a39f5eaf6aaf9e9fee46dd4a447de1
Umaya Thilakarathna <tuchithmini@gmail.com>
2025-03-14 21:52:35 +0530

    DOne the registration part

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { memberApiSlice } from "./member/MemberApiSlice";
 import { bookApiSlice } from "./book/BookApiSlice";
+import { AuthApiSlice } from "./Auth/AuthApiSlice";
 
 export const store = configureStore({
   reducer: {
     [memberApiSlice.reducerPath]: memberApiSlice.reducer,
     [bookApiSlice.reducerPath]: bookApiSlice.reducer,
+    [AuthApiSlice.reducerPath]: AuthApiSlice.reducer, 
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(
       memberApiSlice.middleware,
-      bookApiSlice.middleware
+      bookApiSlice.middleware,
+      AuthApiSlice.middleware 
     );
   },
 });
